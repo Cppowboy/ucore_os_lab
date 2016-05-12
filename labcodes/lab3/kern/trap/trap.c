8e0a02f91d70d1b25143ef96b5ad14a53eb029de
cppowboy <cppowboy@outlook.com>
2016-05-12 21:03:50 +0800

    finish lab3 code

--- labcodes/lab3/kern/trap/trap.c
+++ labcodes/lab3/kern/trap/trap.c
@@ -48,6 +48,14 @@ idt_init(void) {
       *     You don't know the meaning of this instruction? just google it! and check the libs/x86.h to know more.
       *     Notice: the argument of lidt is idt_pd. try to find it!
       */
+	extern uintptr_t __vectors[];
+	int i;
+	for (i = 0; i < 256; i ++)
+	{
+		SETGATE(idt[i],0,GD_KTEXT,__vectors[i],0);
+	}
+	SETGATE(idt[T_SWITCH_TOK],0,GD_KTEXT,__vectors[T_SWITCH_TOK],3);
+	lidt(&idt_pd);
 }
 
 static const char *
@@ -186,6 +194,9 @@ trap_dispatch(struct trapframe *tf) {
          * (2) Every TICK_NUM cycle, you can print some info using a funciton, such as print_ticks().
          * (3) Too Simple? Yes, I think so!
          */
+		ticks++;
+		if(ticks%TICK_NUM==0)
+			print_ticks();
         break;
     case IRQ_OFFSET + IRQ_COM1:
         c = cons_getc();
